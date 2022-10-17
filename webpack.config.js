f89ad800455ef8ee17c3612fcd927b756efbddf6
Ирина Белова <i.m.belova@mail.ru>
2022-10-17 16:18:48 +0300

    some new

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV === 'development';
 module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    },
+    mode: NODE_ENV ? NODE_ENV : 'development',
     entry: path.resolve(__dirname, 'src/index.jsx'),
     output: {
-        path: path.resolve(__dirname, 'dict'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'index.js'
+    },
+    module: {
+        rules: [{
+            test: /\.[tj]sx?$/,
+            use: ['ts-loader']
+        }]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({template: path.resolve(__dirname, 'index.html')})
+    ],
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: IS_DEV
     }
+
 };
