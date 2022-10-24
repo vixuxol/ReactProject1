301bc1dd017eec2b1671d092b372c353955bbf69
Irina Belova <i.m.belova@mail.ru>
2022-10-24 18:52:57 +0300

    Добавлен компонент CardsList хранилище списка карточек

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { hot } from 'react-hot-loader/root';
 import { Layout } from './shared/Layout/Layout';
 import { Header } from './shared/Header/Header'
 import { Content } from './shared/Content/Content'
+import { CardsList } from './shared/CardsList/CardsList'
 
 function AppComponent() {
     return (
         <Layout>
             <Header />
             <Content>
-                content
+                <CardsList></CardsList>
             </Content>
         </Layout>
     );
---
+++ src/shared/CardsList/CardsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './cardslist.css';
+
+export function CardsList() {
+  return (
+    <ul className = {styles.cardslist}>
+      <li> one </li>
+      <li> two </li>
+    </ul>
+  );
+}
