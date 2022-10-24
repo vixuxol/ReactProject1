2c44a12d71ef475d4507515e86238b53853b22eb
Irina Belova <i.m.belova@mail.ru>
2022-10-25 01:38:43 +0300

    Добавлена область превью

--- src/shared/CardsList/Card/Card.tsx
+++ src/shared/CardsList/Card/Card.tsx
@@ -20,7 +20,12 @@ export function Card() {
           <a href = "#post-url" className = {styles.postLink}>Следует, отметить, что новая модель организационной деятельности....</a>
         </h2>
       </div>
-      <div className = {styles.preview}></div>
+      <div className = {styles.preview}>
+        <img 
+          className = {styles.previewImg}
+          src = "https://cdn.dribbble.com/users/4567268/screenshots/19724290/media/b154bf6942032e9d68051287f435d960.png?compress=1&resize=450x338&vertical=top"
+      />
+      </div>
       <div className = {styles.menu}></div>
       <div className = {styles.content}></div>
     </li>
