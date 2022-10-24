dcee73abc6c80a3c3737f0415a92f1df05a5f88c
Irina Belova <i.m.belova@mail.ru>
2022-10-24 19:13:29 +0300

    Добавлен пустой компонент Card

---
+++ src/shared/CardsList/Card/Card.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './card.css';
+
+export function Card() {
+  return (
+    <li className = {styles.card}></li>
+  );
+}
--- src/shared/CardsList/CardsList.tsx
+++ src/shared/CardsList/CardsList.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './cardslist.css';
+import { Card } from './Card'
 
 export function CardsList() {
   return (
     <ul className = {styles.cardslist}>
-      <li> one </li>
-      <li> two </li>
+      <Card />
     </ul>
   );
 }
