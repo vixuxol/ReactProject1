fa20003eb13927e350e44ef992bec03b81c38652
Irina Belova <i.m.belova@mail.ru>
2022-10-27 12:04:27 +0300

    Вынесена иконка Меню в отдельный компонент

---
+++ src/shared/icons/MenuIcon.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export function MenuIcon() {
+    return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="5" height="20" viewBox="0 0 5 20" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z"/>
+      <circle cx="2.5" cy="2.5" r="2.5" fill = "#D9D9D9"/>
+      <circle cx="2.5" cy="10" r="2.5" fill = "#D9D9D9" />
+      <circle cx="2.5" cy="17.5" r="2.5" fill = "#D9D9D9"/>
+    </svg>
+    );
+}
