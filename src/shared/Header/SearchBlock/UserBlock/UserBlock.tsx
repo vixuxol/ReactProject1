ff098777b7379837964959f63f3e98d9047cda34
Irina Belova <i.m.belova@mail.ru>
2022-11-04 01:49:23 +0300

    Изменения для клиент-id

--- src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -10,7 +10,9 @@ interface IUserBlockProps {
 
 export function UserBlock({ avatarSrc, username }: IUserBlockProps) {
   return (
-    <a className = {styles.userBox}>
+    <a  href = {`https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=token&state=random_string&redirect_uri=http://localhost:3000/auth&scope=read submit identity`}
+        className = {styles.userBox}
+    >
       <div className = {styles.avatarBox}>
         {avatarSrc
           ? <img src = {avatarSrc} alt = "user avatar" className = {styles.avatarImage} />
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const { DefinePlugin } = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
 const GLOBAL_CSS_REGEXP = /\.global\.css$/;
+//const DEV_PLUGINS =  new HTMLWebpackPlugin({template: path.resolve(__dirname, 'index.html')})
+//const COMMON_PLUGINS = [ new DefinePlugin({ 'process.env.CLIENT_ID': `'${process.env.CLIENT_ID}'`})]
 module.exports = {
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
@@ -38,9 +41,8 @@ module.exports = {
         }
     ]
     },
-    plugins: [
-        new HTMLWebpackPlugin({template: path.resolve(__dirname, 'index.html')})
-    ],
+    plugins: [new HTMLWebpackPlugin({template: path.resolve(__dirname, 'index.html')}), new DefinePlugin({ 'process.env.CLIENT_ID': `'${process.env.CLIENT_ID}'`})],
+    
     devServer: {
         port: 3000,
         open: true,
