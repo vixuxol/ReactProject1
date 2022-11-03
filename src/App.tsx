cff54c367a154f7a25a8c15a199b0021d6c8a0d4
Irina Belova <i.m.belova@mail.ru>
2022-11-03 13:34:05 +0300

    Добавлены изменения формирующие DropDown меню

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,14 @@ import { Layout } from './shared/Layout';
 import { Header } from './shared/Header'
 import { Content } from './shared/Content'
 import { CardsList } from './shared/CardsList'
-import { Dropdown } from './shared/Dropdown';
-//import { generateId, generateRandomString } from './utils/react/generateRandomIndex';
-//import { GenericList } from './lessons/GenericList'
-//import { merge } from './utils/js/merge'
-
-// константа, которая содержит элементы нашего списка (надо к ним добавить id)
-// лучше константу выносить вне рендера компонента (один раз сделать и сохранить)
-// чтобы делать что-то с константным списком - нужно сделать состояние 
-// const LIST = [
-//     { text: 'some' },
-//     { text: 'other value' },
-//     { text: 'one more value' },
-// ].map(generateId) // в таком исполнении мы сгенерим id один раз и всем элементам будет присвоен один и тот же id
-// хотя кажется, что это не так (до этого был assignId)
 
 function AppComponent() {
-//     const[list, setList] = React.useState(LIST); // в аргументах - начальное состояние
     
-//     const handleItemClick = (id:string) => {
-//         setList(list.filter((item) => item.id != id));
-//     }
-//     const handleAdd = () => {
-//         setList(list.concat(generateId({ text: generateRandomString() })))
-//     }
     return (
         <Layout>
             <Header />
             <Content>
                 <CardsList />
-                <div style={{padding: 20}}>
-                    <br />
-                    <Dropdown isOpen = {false} onClose = {() => console.log('closed')} onOpen = {() => console.log('isopen')} button = {<button>Test</button>}>
-                        <ul>
-                            <li onClick = {console.log}>Click me</li>
-                            <li> Dont Click me</li>
-                        </ul>
-                    </Dropdown>
-                </div>
             </Content>
         </Layout>
     );
--- src/shared/CardsList/Card/Menu/Menu.tsx
+++ src/shared/CardsList/Card/Menu/Menu.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Dropdown } from '../../../Dropdown';
 import { MenuIcon } from '../../../icons';
+import { EColor, Text } from '../../../Text'
 import styles from './menu.css';
+import { MenuItemsList } from './MenuItemsList';
 
 export function Menu() {
   return (
     <div className = {styles.menu}>
-    <button className = {styles.menuButton}>
-      <MenuIcon />
-    </button>
+    
+    <Dropdown
+      button = {
+        <button className = {styles.menuButton}>
+        <MenuIcon />
+        </button>
+      }
+    >
+      <div className = {styles.dropdown}>
+          <MenuItemsList postId='1'/>
+          <button className = {styles.closeButton}>
+            <Text mobileSize = {12} size = {14} color = {EColor.grey66}>
+              Закрыть
+            </Text>
+          </button>
+      </div>
+    </Dropdown>
   </div>
   );
 }
